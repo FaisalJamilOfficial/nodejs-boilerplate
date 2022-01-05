42fe640062e9d90260eaf969c489f878dc6b6569
Faisal Jamil <faisaljamil606@gmail.com>
2022-01-05 10:01:20 +0500

    updated mailer

--- utils/Mailer.js
+++ utils/Mailer.js
@@ -3,8 +3,8 @@ const nodemailer = require("nodemailer");
 const sendEmail = async (email, subject, text) => {
 	try {
 		const transporter = nodemailer.createTransport({
-			host: process.env.EMAIL_HOST,
-			service: process.env.EMAIL_SERVICE,
+			host: "smtp.gmail.com",
+			service: "gmail",
 			port: 587,
 			secure: true,
 			auth: {

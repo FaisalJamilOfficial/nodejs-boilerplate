242037b7a91b392665e409116d96739905c977dc
Faisal Jamil Khan <faisaljamil606@gmail.com>
2022-12-12 13:04:53 +0500

    fixed directory issues

--- src/configs/directories.js
+++ src/configs/directories.js
@@ -1,8 +1,7 @@
 const __basedir = __dirname.toString().replace("configs", "");
 
 module.exports = {
-	PUBLIC_DIRECTORY: __basedir + "public/",
-	IMAGES_DIRECTORY: __basedir + "/public/images/",
-	PROFILE_PICTURES_DIRECTORY: __basedir + "public/images/profile_pictures/",
-	ATTACHMENTS_DIRECTORY: __basedir + "public/attachments/",
+  PUBLIC_DIRECTORY: __basedir + "public/",
+  IMAGES_DIRECTORY: __basedir + "/public/images/",
+  ATTACHMENTS_DIRECTORY: __basedir + "public/attachments/",
 };
--- src/utils/FilesDeleter.js
+++ src/utils/FilesDeleter.js
@@ -1,59 +1,63 @@
 const fs = require("fs");
 
 const {
-	IMAGES_DIRECTORY,
-	ATTACHMENTS_DIRECTORY,
+  IMAGES_DIRECTORY,
+  ATTACHMENTS_DIRECTORY,
 } = require("../configs/directories");
 
 class FilesDeleter {
-	constructor() {
-		this.fs = fs;
-	}
+  constructor() {
+    this.fs = fs;
+  }
 
-	/**
-	 * Delete image file
-	 * @param {string} image image file name
-	 * @returns {null}
-	 */
-	async deleteImage(parameters) {
-		const { image } = parameters;
-		const array = [];
-		const PATH = IMAGES_DIRECTORY;
-		array.push({ path: PATH + image });
-		array.push({ path: PATH + "thumbnails/" + image });
-		this.deleteFiles({ files: array });
-		return;
-	}
+  /**
+   * Delete image file
+   * @param {string} image image file name
+   * @returns {null}
+   */
+  async deleteImage(parameters) {
+    const { image } = parameters;
+    const array = [];
+    const PATH = IMAGES_DIRECTORY;
+    array.push({ path: PATH + image });
+    array.push({ path: PATH + "thumbnails/" + image });
+    this.deleteFiles({ files: array });
+    return;
+  }
 
-	/**
-	 * Delete attachment file
-	 * @param {string} attachment attachment file name
-	 * @returns {null}
-	 */
-	async deleteAttachment(parameters) {
-		const { attachment } = parameters;
-		const array = [];
-		const PATH = ATTACHMENTS_DIRECTORY;
-		array.push({ path: PATH + attachment });
-		this.deleteFiles({ files: array });
-		return;
-	}
+  /**
+   * Delete attachment file
+   * @param {string} attachment attachment file name
+   * @returns {null}
+   */
+  async deleteAttachment(parameters) {
+    const { attachment } = parameters;
+    const array = [];
+    const PATH = ATTACHMENTS_DIRECTORY;
+    array.push({ path: PATH + attachment });
+    this.deleteFiles({ files: array });
+    return;
+  }
 
-	/**
-	 * Delete files from server
-	 * @param {[object]} files array of files
-	 * @returns {null}
-	 */
-	async deleteFiles(parameters) {
-		const { files } = parameters;
-		if (files && Array.isArray(files)) {
-			for (let i = 0; i < files.length; i++) {
-				const element = files[i];
-				fs.unlinkSync(element.path);
-			}
-		}
-		return;
-	}
+  /**
+   * Delete files from server
+   * @param {[object]} files array of files
+   * @returns {null}
+   */
+  async deleteFiles(parameters) {
+    const { files } = parameters;
+    if (files && Array.isArray(files)) {
+      for (let i = 0; i < files.length; i++) {
+        const element = files[i];
+        try {
+          fs.unlinkSync(element.path);
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    }
+    return;
+  }
 }
 
 module.exports = FilesDeleter;

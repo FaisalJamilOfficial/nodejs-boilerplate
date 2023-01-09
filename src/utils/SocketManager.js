e8cc14c3111ccae7831518033f82b56e3e006ca8
Faisal Jamil Khan <faisaljamil606@gmail.com>
2023-01-09 19:05:59 +0500

    updated socket manager

--- src/utils/SocketManager.js
+++ src/utils/SocketManager.js
@@ -6,36 +6,47 @@
 // });
 
 class SocketManager {
-	constructor(io) {
-	  this.io = io;
-	  // this.connection = connection;
-	}
-  
-	/**
-	 * Emit event
-	 * @param {string} to event listener
-	 * @param {string} event event title
-	 * @param {object} data event data
-	 * @returns {object} socket response
-	 */
-	async emitEvent(parameters) {
-	  const { to, event, data } = parameters;
-	  const response = await io.to(to).emit(event, data);
-	  // const response = await connection
-	  //   .firestore()
-	  //   .collection("socket")
-	  //   .doc(to)
-	  //   .set(
-	  //     JSON.parse(
-	  //       JSON.stringify({
-	  //         type: event,
-	  //         data,
-	  //       })
-	  //     )
-	  //   );
-	  return response;
-	}
+  constructor() {
+    this.io = global.io;
+    // this.connection = connection;
   }
-  
-  module.exports = SocketManager;
-  
+
+  /**
+   * Emit event
+   * @param {string} to event listener
+   * @param {string} event event title
+   * @param {object} data event data
+   * @returns {object} socket response
+   */
+  async emitEvent(parameters) {
+    const { to, event, data } = parameters;
+    let response = await io.to(to).emit(event, data);
+    // response = await connection
+    // 	.firestore()
+    // 	.collection("socket")
+    // 	.doc(to)
+    // 	.set(
+    // 		JSON.parse(
+    // 			JSON.stringify({
+    // 				type: event,
+    // 				data,
+    // 			})
+    // 		)
+    // 	);
+    return response;
+  }
+
+  /**
+   * Emit event
+   * @param {string} event event title
+   * @param {object} data event data
+   * @returns {object} socket response
+   */
+  async emitGroupEvent(parameters) {
+    const { event, data } = parameters;
+    let response = await io.emit(event, data);
+    return response;
+  }
+}
+
+module.exports = SocketManager;

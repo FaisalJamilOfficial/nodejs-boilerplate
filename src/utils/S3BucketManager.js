770d3317ab96a924ae9a20c9099ec4a4b8afc599
Faisal Jamil Khan <faisaljamil606@gmail.com>
2023-01-23 21:16:12 +0500

    updated s3BucketManager

--- src/utils/S3BucketManager.js
+++ src/utils/S3BucketManager.js
@@ -1,20 +1,17 @@
-const AWS = require("aws-sdk");
+const { S3Client, DeleteObjectCommand } = require("@aws-sdk/client-s3");
 const multer = require("multer");
 const multerS3 = require("multer-s3");
 const uuid = require("uuid").v4;
 const mime = require("mime-types");
-const { AWS_NAME, AWS_ACCESS_KEY, AWS_SECRET_KEY, AWS_REGION } = process.env;
-const s3Client = new AWS.S3({
-  // apiVersion: "2006-03-01",
-  accessKeyId: AWS_ACCESS_KEY,
-  secretAccessKey: AWS_SECRET_KEY,
-  // ServerSideEncryption: "AES256",
-  region: AWS_REGION,
+const { AWS_BUCKET_NAME, AWS_ACCESS_KEY, AWS_SECRET_KEY } = process.env;
+
+const s3 = new S3Client({
+  credentials: { accessKeyId: AWS_ACCESS_KEY, secretAccessKey: AWS_SECRET_KEY },
 });
 
 class S3BucketManager {
   constructor() {
-    this.s3Client = s3Client;
+    this.s3 = s3;
   }
 
   /**
@@ -23,54 +20,33 @@ class S3BucketManager {
    */
   upload = multer({
     storage: multerS3({
-      s3: s3Client,
-      bucket: AWS_NAME,
+      s3,
+      bucket: AWS_BUCKET_NAME,
       metadata: (req, file, cb) => {
-        cb(null, { fieldName: file.fieldname });
+        cb(null);
       },
       key: (req, file, cb) => {
-        cb(null, uuid());
+        const fileExtension = "." + mime.extension(file.mimetype);
+        cb(null, uuid() + fileExtension);
       },
     }),
   });
 
-  async deleteAwsObject(url) {
-    const url1 = String(url).split("/");
-    const len = String(url).split("/").length;
-    const uploadParams = {
-      Bucket: AWS_NAME,
-      Key: url1[len - 1], // pass key
-    };
-    s3Client.deleteObject(uploadParams, async (err, data) => {
-      if (err) {
-        console.log("err" + err);
-      } else {
-        console.log("deleted" + JSON.stringify(data));
-      }
-    });
-  }
-
-  async uploadFile(file, directory) {
-    const fileObj = Array.isArray(file) ? file[0] : file;
-    let fileName = uuid();
-    const fileExtension = mime.extension(fileObj.mimetype);
-    fileName += "." + fileExtension;
-    if (directory) fileName = `${directory}/${fileName}`;
-    const uploadParams = {
-      Bucket: AWS_NAME,
-      Key: fileName, // pass key
-      ContentType: fileObj.mimetype,
-      Body: fileObj.buffer, // pass file body
-    };
-    return await s3Client
-      .upload(uploadParams)
-      .promise()
-      .then((data) => {
-        return data.Key;
-      })
-      .catch((e) => {
-        console.log("e", e);
-      });
+  /**
+   *
+   * @param {string} filePath path to file
+   * @returns {object} data of deleted object
+   */
+  async delete(params) {
+    const { path } = params;
+    if (path);
+    else return null;
+    const keyArray = path.split("/");
+    const key = keyArray[keyArray.length - 1];
+    const input = { Bucket: AWS_BUCKET_NAME, Key: key };
+    const command = new DeleteObjectCommand(input);
+    const data = await s3.send(command);
+    return data;
   }
 }
 

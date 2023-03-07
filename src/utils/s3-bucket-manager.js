f12e7102b9672d804d9efbeaa7512f7c20a5addf
Faisal Jamil Khan <faisaljamil606@gmail.com>
2023-03-07 15:27:02 +0500

    Update s3-bucket-manager.js

--- src/utils/s3-bucket-manager.js
+++ src/utils/s3-bucket-manager.js
@@ -1,5 +1,9 @@
 // module imports
-import { S3Client, DeleteObjectCommand } from "@aws-sdk/client-s3";
+import {
+  S3Client,
+  DeleteObjectCommand,
+  CopyObjectCommand,
+} from "@aws-sdk/client-s3";
 import multer from "multer";
 import multerS3 from "multer-s3";
 import { v4 } from "uuid";
@@ -37,7 +41,7 @@ class S3BucketManager {
   });
 
   /**
-   *
+   * @description delete s3 bucket object
    * @param {String} filePath path to file
    * @returns {Object} data of deleted object
    */
@@ -49,8 +53,24 @@ class S3BucketManager {
     const key = keyArray[keyArray.length - 1];
     const input = { Bucket: AWS_BUCKET_NAME, Key: key };
     const command = new DeleteObjectCommand(input);
-    const data = await s3.send(command);
-    return data;
+    return await s3.send(command);
+  }
+
+  /**
+   * @description copy s3 bucket object
+   * @param {String} sourceFile path to source file
+   * @param {String} destinationKey key of destination file
+   * @returns {Object} data of copied object
+   */
+  async copy(params) {
+    const { sourceFile, destinationKey } = params;
+    const input = {
+      Bucket: AWS_BUCKET_NAME,
+      CopySource: sourceFile,
+      Key: destinationKey,
+    };
+    const command = new CopyObjectCommand(input);
+    return await s3.send(command);
   }
 }
 

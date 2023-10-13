4462d905ebd2fb5d5dcf35d3cf4089ba6d424760
Faisal Jamil Khan <133661997+FaisalJamil-DITS@users.noreply.github.com>
2023-10-13 19:47:06 +0500

    Update users.js

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -99,7 +99,8 @@ export const updateUser = async (params) => {
   if (firstName) userExists.firstName = firstName;
   if (lastName) userExists.lastName = lastName;
   if (firstName || lastName)
-    userExists.name = userExists.firstName + " " + userExists.lastName;
+    userExists.name =
+      (userExists.firstName ?? "") + " " + (userExists.lastName ?? "");
   if (image) {
     if (userExists.image)
       new FilesDeleter().deleteImage({ image: userExists.image });

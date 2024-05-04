ff9240143e4e40a636c6cb4cce6836c313163eb5
Faisal Jamil <133661997+FaisalJamil-DITS@users.noreply.github.com>
2024-05-04 09:22:30 +0500

    Update test.js

--- src/test-suites/test.js
+++ src/test-suites/test.js
@@ -1,12 +1,12 @@
 // module imports
-// import { describe, expect, test } from "@jest/globals";
+import { describe, expect, test } from "@jest/globals";
 
 const getAppTitle = () => {
   return "Backend Boilerplate";
 };
 
-// describe("get app title", () => {
-//   test("returns App Title as Boilerplate", () => {
-//     expect(getAppTitle()).toBe("Boilerplate");
-//   });
-// });
+describe("get app title", () => {
+  test("returns App Title as Boilerplate", () => {
+    expect(getAppTitle()).toBe("Boilerplate");
+  });
+});
